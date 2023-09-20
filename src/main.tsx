7b0489888da9c13d1deaeb5f9b49ac22ea9d4c11
Rockielitte <truongquang2121@gmail.com>
2023-09-20 23:16:02 +0700

    some init login

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='949635241718-0qudmklc1b9ts3e4i8ldcui54l12hmuk.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 )
--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,9 +1,29 @@
 import { FC } from 'react'
-
+import { GoogleLogin } from '@react-oauth/google'
+import videoBg from '@/assets/bg-video.mp4'
+import { Button } from '@/components/ui/button'
 const Login: FC = () => {
-  console.log('run2')
-
-  return <div>Login</div>
+  return (
+    <div className='w-screen h-screen flex relative justify-center items-center bg-white'>
+      <div className='absolute  inset-0 opacity-50'>
+        <video src={videoBg} className='w-full h-full object-cover' autoPlay muted loop />
+      </div>
+      <div className='z-10 flex flex-col gap-5'>
+        <h1 className='bg-gradient-to-tr from-orange-600 to-slate-50 text-transparent bg-clip-text '>
+          <span className=' text-black font-sans text-5xl font-extrabold'>Sign in to join </span>
+          <span className='uppercase text-7xl font-extrabold '>zoocamp</span>
+        </h1>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse)
+          }}
+          onError={() => {
+            console.log('Login Failed')
+          }}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Login
