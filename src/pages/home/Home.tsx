0fd98b30945fed9750cb7c9f1a944c78f7f85f5d
List Kelvin <phamm5687@gmail.com>
2023-09-30 10:00:09 +0700

    test theme

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react'
 import Banner from './banner/Banner'
+import { useTheme } from '@/components/theme-provider'
 interface HomeProps {}
 const Home: FC<HomeProps> = () => {
+  const { setTheme } = useTheme()
   return (
     <div>
       <Banner />
+
+      <button type='button' onClick={() => setTheme('light')}>
+        light
+      </button>
+      <br />
+      <button type='button' onClick={() => setTheme('dark')}>
+        dark
+      </button>
     </div>
   )
 }
