2c10f5dcbef4c60c30e267db6dfe2f0dea2a92e3
Your Name <thotv.t1.1821@gmail.com>
2023-11-11 08:07:32 +0700

    update notification success

--- src/pages/tickets/SuccessNoti.tsx
+++ src/pages/tickets/SuccessNoti.tsx
@@ -7,9 +7,7 @@ import {
   DialogHeader,
   DialogTitle
 } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Order } from '@/types'
+import Email from '@/utils/api/Email'
 import MyOrder from '@/utils/api/MyOrder'
 import Payment from '@/utils/api/Payment'
 import { useEffect, useState } from 'react'
@@ -17,8 +15,6 @@ import { useLocation, useNavigate } from 'react-router-dom'
 
 const SuccessNoti = () => {
   const [open, setOpen] = useState<boolean>(true)
-  const [email, setEmail] = useState<string>('example@gmail.com')
-  const [orderId, setOrderId] = useState<string>('example@ID')
   const [isValidReturnUrl, setIsValidReturnUrl] = useState<boolean>(false)
 
   const navigate = useNavigate()
@@ -31,11 +27,6 @@ const SuccessNoti = () => {
   }
 
   useEffect(() => {
-    // function getOrderId() {
-    //   if (new URLSearchParams(search).get('vnp_TxnRef') as string) {
-    //     setOrderId(new URLSearchParams(search).get('vnp_TxnRef') as string)
-    //   }
-    // }
     async function processOrderValid() {
       try {
         const url = window.location.href
@@ -43,22 +34,11 @@ const SuccessNoti = () => {
         if (response.data) {
           const orderIdOnParams = new URLSearchParams(search).get('vnp_TxnRef') as string
           if (orderIdOnParams) {
-            const response = await MyOrder.getOrder(orderIdOnParams)
-
-            setEmail(response.data.email)
-
-            setOrderId(new URLSearchParams(search).get('vnp_TxnRef') as string)
+            Promise.all([Email.sendQrCode(orderIdOnParams), MyOrder.updateOrderStatus(orderIdOnParams, 'DONE')])
 
             setIsValidReturnUrl(true)
 
-            const orderString = localStorage.getItem('order')
-            if (orderString) {
-              const order = JSON.parse(orderString) as Order
-              if (orderIdOnParams == order.id) {
-                await MyOrder.updateOrderStatus(orderIdOnParams, 'DONE')
-                localStorage.removeItem('order')
-              }
-            }
+            localStorage.removeItem('order')
           }
         }
       } catch (error) {
@@ -66,74 +46,17 @@ const SuccessNoti = () => {
       }
     }
 
-    // async function updateOrderStatus() {
-    //   const orderIdOnParams = new URLSearchParams(search).get('vnp_TxnRef') as string
-
-    //   const orderString = localStorage.getItem('order')
-    //   if (orderString) {
-    //     const order = JSON.parse(orderString) as Order
-
-    //     if (orderIdOnParams && orderIdOnParams == order.id) {
-    //       await MyOrder.updateOrderStatus(orderIdOnParams, 'DONE')
-    //       localStorage.removeItem('order')
-    //     }
-    //   }
-    // }
-
-    // getOrderId()
     processOrderValid()
-    // updateOrderStatus()
   }, [])
   return (
     <div className='h-screen flex items-center justify-center'>
-      {isValidReturnUrl ? (
+      {isValidReturnUrl && (
         <Dialog open={open} onOpenChange={() => handleCloseDialog()}>
-          <DialogContent className='sm:max-w-[500px] min-h-[350px]'>
+          <DialogContent className='sm:max-w-[400px] min-h-[200px]'>
             <DialogHeader>
               <DialogTitle>Order successfully</DialogTitle>
               <DialogDescription>Order information will be sent to your email</DialogDescription>
             </DialogHeader>
-            <div className='grid gap-4 py-4'>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label htmlFor='name' className='text-right'>
-                  Order ID
-                </Label>
-                <Input value={orderId} id='name' defaultValue='Pedro Duarte' className='col-span-3' />
-              </div>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label htmlFor='username' className='text-right'>
-                  Email
-                </Label>
-                <Input value={email} id='username' defaultValue='@peduarte' className='col-span-3' />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button onClick={() => handleCloseDialog()} type='submit'>
-                OK
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      ) : (
-        <Dialog open={open} onOpenChange={() => handleCloseDialog()}>
-          <DialogContent className='sm:max-w-[500px] min-h-[350px]'>
-            <DialogHeader>
-              <DialogTitle className='text-red-600'>Invalid Order</DialogTitle>
-            </DialogHeader>
-            <div className='grid gap-4 py-4'>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label htmlFor='name' className='text-right'>
-                  Order ID
-                </Label>
-                <Input value={orderId} id='name' defaultValue='Pedro Duarte' className='col-span-3' />
-              </div>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label htmlFor='username' className='text-right'>
-                  Email
-                </Label>
-                <Input value={email} id='username' defaultValue='@peduarte' className='col-span-3' />
-              </div>
-            </div>
             <DialogFooter>
               <Button onClick={() => handleCloseDialog()} type='submit'>
                 OK
