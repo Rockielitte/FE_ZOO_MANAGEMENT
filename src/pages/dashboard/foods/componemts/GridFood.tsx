75b4f8aba698294052a717fa849b1128864bf2f8
longlt201203 <longlt201203@gmail.com>
2023-11-18 05:49:37 +0700

    ngĩn

--- src/pages/dashboard/foods/componemts/GridFood.tsx
+++ src/pages/dashboard/foods/componemts/GridFood.tsx
@@ -1,33 +1,31 @@
-import { SpeciesCard } from '@/components/SpeciesCard'
-import { User, dataSpecies, food } from '@/types'
+// import { SpeciesCard } from '@/components/SpeciesCard'
+import { User, food } from '@/types'
 import { Table } from '@tanstack/react-table'
-import Lin from '@/assets/image14.png'
+// import Lin from '@/assets/image14.png'
 import React from 'react'
 import { FoodCard } from './FoodCard'
 
 const GridFood: React.FC<{
-    data: Table<food>
-    user: User
-
-
+  data: Table<food>
+  user: User
 }> = ({ data, user }) => {
-    console.log("data: ", data);
-    return (
-        <div className='grid grid-cols-1 gap-x-4 gap-y-7 lg:grid-cols-4 p-4'>
-            {data.getRowModel().rows.map((row) => {
-                return (
-                    <FoodCard
-                        key={row.original.id}
-                        id={row.original.id}
-                        name={row.original.name}
-                        type={row.original.type}
-                        unit={row.original.unit}
-                        description={row.original.description}
-                        user={user}
-                    />
-                )
-            })}
-            {/* {data.map((row) => {
+  console.log('data: ', data)
+  return (
+    <div className='grid grid-cols-1 gap-x-4 gap-y-7 lg:grid-cols-4 p-4'>
+      {data.getRowModel().rows.map((row) => {
+        return (
+          <FoodCard
+            key={row.original.id}
+            id={row.original.id}
+            name={row.original.name}
+            type={row.original.type}
+            unit={row.original.unit}
+            description={row.original.description}
+            user={user}
+          />
+        )
+      })}
+      {/* {data.map((row) => {
                 return (
                     <FoodCard
                         key={row.id}
@@ -41,8 +39,8 @@ const GridFood: React.FC<{
                     />
                 )
             })} */}
-        </div>
-    )
+    </div>
+  )
 }
 
 export default GridFood
