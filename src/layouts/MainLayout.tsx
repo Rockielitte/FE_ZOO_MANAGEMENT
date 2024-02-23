03f98961b9fa989845924880f7f068788e885395
Phạm Công Minh <phamm5687@gmail.com>
2024-02-23 15:55:24 +0700

    chore: test github action

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -21,7 +21,8 @@ const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   const [isShow, setIsShow] = useState(true)
   const transitions = useTransition(isShow, {
     from: { opacity: 0, transform: 'translateX(-100%)' },
-    enter: { opacity: 1, transform: 'translateX(0%)' }
+    enter: { opacity: 1, transform: 'translateX(0%)' },
+    leave: { opacity: 0, transform: 'translateX(-100%)' }
   })
   const { theme } = useTheme()
   const url = useLocation().pathname
